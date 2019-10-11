758da0b9d916f0540c62a0368883b7c60cd4149f
radua <radua@outlook.com>
2019-10-11 15:13:11 +0000

    Display errors with parameter

--- Shipping/Exceptions/SmartshipException.php
+++ Shipping/Exceptions/SmartshipException.php
@@ -3,29 +3,34 @@ namespace Flagship\Shipping\Exceptions;
 
 class SmartshipException extends \Exception{
 
-    protected function getErrors() : array { 
-       
+    protected function getErrors() : array {
+
         if(empty($this->message)){
 
-            $this->message = 'Unable to connect to FlagShip - Code : '. $this->getCode();           
+            $this->message = 'Unable to connect to FlagShip - Code : '. $this->getCode();
             $errorsArray = [ $this->message ];
             return $errorsArray;
         }
 
-        if(!$this->isJson($this->message) || is_string($this->message)){ 
-            
+        if(!$this->isJson($this->message)){
+
             $this->message = parent::getMessage();
             $errorsArray = [ $this->message ];
             return $errorsArray;
         }
 
+        if(is_string($this->message) && is_array(json_decode($this->message,TRUE)) && count(json_decode($this->message,TRUE)) == 1){
+            $errorsArray = [$this->message];
+            return $errorsArray;
+        }
+
         $errors = json_decode($this->message,TRUE)["errors"];
-        
+
         if(is_null($errors)){
             $errorsArray = [ $this->message ];
             return $errorsArray;
         }
-        
+
         if(is_string($errors)){
             $errorsArray = [ $errors ];
             return $errorsArray;
@@ -38,7 +43,7 @@ class SmartshipException extends \Exception{
 
         $errorsArray = [];
         $i = 0;
-        $keys = array_keys($errors);      
+        $keys = array_keys($errors);
 
         foreach($errors as $error){
 
@@ -46,7 +51,7 @@ class SmartshipException extends \Exception{
             $i++;
         }
         return $errorsArray;
-        
+
     }
 
     protected function normalizeErrors(array $error) : string {
@@ -59,7 +64,7 @@ class SmartshipException extends \Exception{
         return $errorMsg;
     }
 
-    protected function isJson(string $string) : bool { 
+    protected function isJson(string $string) : bool {
         json_decode($string);
         return (json_last_error() === JSON_ERROR_NONE);
     }
@@ -75,13 +80,13 @@ class SmartshipException extends \Exception{
 
     protected function getResponseErrorsByType($type) : array {
         $errors = json_decode($this->message,TRUE)[$type];
-        
+
         if(is_string($errors)){
             return [ $errors ];
         }
 
         $responseErrorMsg = [];
-    
+
         while(!is_string($errors)){
             $responseErrorMsg[] = is_string(key($errors)) ? key($errors)." : " : "";
             $errors = reset($errors);
